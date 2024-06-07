4acebebdad3ee7ea3e1ca5ad98d72cc8483a2309
j-miniee <potatopizza0518@naver.com>
2024-06-07 12:42:55 +0900

    24-06-07

---
+++ baekjoon_C/silver/Lv3_#4375_모든 자리수가 1/모든 자릿수가 1.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n, i;
+	char m[10000];
+
+	i = 0;
+	while (1) {
+		m[i] = 1;
+		atoi(m);
+
+		//if()
+	}
+
+
+
+
+
+}
